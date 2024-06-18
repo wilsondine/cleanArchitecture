4430bc625748488592d052ed134fdb4fd15c5812
Wilson Dine de Macedo <wdine71@hotmail.com>
2024-06-18 15:53:06 -0300

    endpoint para Products

--- src/infrastructure/api/express.ts
+++ src/infrastructure/api/express.ts
@@ -3,9 +3,16 @@ import { Sequelize } from "sequelize-typescript";
 import CustomerModel from "../customer/repository/sequelize/customer.model";
 import { customerRoute } from "./routes/customer.route";
 
+import ProductModel from "../product/repository/sequelize/product.model";
+import { productRoute } from "./routes/product.route";
+
 export const app: Express = express();
 app.use(express.json());
 app.use("/customer", customerRoute);
+
+export const app2: Express = express();
+app2.use(express.json());
+app2.use("/product", productRoute);
 
 export let sequelize: Sequelize;
 
@@ -16,6 +23,7 @@ async function setupDb() {
     logging: false,
   });
   await sequelize.addModels([CustomerModel]);
+  await sequelize.addModels([ProductModel]);
   await sequelize.sync();
 }
 setupDb();
